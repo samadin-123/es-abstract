7d8b19c7885aaa5e36769258f16f3a393d89bfed
polytest <polytest@polyresearch.dev>
2026-04-23 09:31:04 +0200

    polyresearch: strip files outside editable surface

--- 2025/ToString.js
+++ 2025/ToString.js
@@ -8,16 +8,7 @@ var $TypeError = require('es-errors/type');
 // https://262.ecma-international.org/6.0/#sec-tostring
 
 module.exports = function ToString(argument) {
-	var argType = typeof argument;
-	// Fast path for common primitive types that don't need conversion
-	if (argType === 'string') {
-		return argument;
-	}
-	if (argType === 'number') {
-		return $String(argument);
-	}
-	// Check for symbol before calling $String
-	if (argType === 'symbol') {
+	if (typeof argument === 'symbol') {
 		throw new $TypeError('Cannot convert a Symbol value to a string');
 	}
 	return $String(argument);
